fe2c5a087ee1918b9e7b4e62cec4d46e8decea88
kumar08401 <kumar08401@gmail.com>
2026-03-15 23:04:54 -0400

    test

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Python Development", "Content Creation", "Technical Support" , "Cloud Technologies", "Artificial Intelligence", "XR Technologies"],
+    strings: ["Devops ", "Cloud Technologies", "Solvency.Inc"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
